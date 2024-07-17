7815008b7b021232931ed7e75466680420cad4af
InoussaLouqmane <inoussalouqmane@gmail.com>
2024-07-17 12:15:38 +0100

    Page de Maintenance

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"
+        defer></script>
+    <link rel="stylesheet" href="style.css">
+
+    <title>JTEK Solutions</title>
+</head>
+
+<body>
+
+    <main>
+
+        <div >
+            <img src="./assets/img/logos/logo-blanc.png" class="img-fluid" alt="Logo de JTEK Solutions">
+            <p class="maintenance-text">Site en cours de maintenance</p>
+        </div>
+
+
+
+    </main>
+    <div class="position-absolute bottom-0 start-0 p-0">
+          <img src="./assets/img/pattern.png" alt="">
+        </div>
+</body>
+
+</html>
